f1d5ed1d3428e6d40e7355ede4697fec83aa010e
pedrogf84 <pedrogonzalezfernandez@gmail.com>
2022-05-04 15:54:48 +0200

    patch user

--- src/api/users/user.routes.js
+++ src/api/users/user.routes.js
@@ -2,7 +2,7 @@ const UserRoutes = require("express").Router();
 const { register, login, logout, getCompletedActivities, addActivity, getOne } = require("./user.controller");
 const { isAuth } = require("../../middlewares/auth.middleware");
 
-UserRoutes.get("/:_id", [isAuth], getOne);
+UserRoutes.get("/:_id", getOne);
 UserRoutes.post("/register", register);
 UserRoutes.post("/login", login);
 UserRoutes.post("/logout", [isAuth], logout);
