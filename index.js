80460c6ee206babf77359869e1c32677f46f8ce3
pedrogf84 <pedrogonzalezfernandez@gmail.com>
2022-05-04 11:32:10 +0200

    change headers

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use(
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH");
   res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Origin", "*");
   next();
 });
